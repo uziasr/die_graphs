5711531f012e4023fcffd1c5894ded17ce1767ec
uziasr <40837265+uziasr@users.noreply.github.com>
2018-10-01 14:45:04 -0700

    Update die_chart.py
    Cleared empty spaces and deleted some comments

--- die_chart.py
+++ die_chart.py
@@ -3,14 +3,11 @@ import pygal
 from die import Die
 
 # Create as many die and enter the ones you want to use into die_list
-
 #Argument here can be changed to a different numbered die
 die_1 = Die(6)
 die_2 = Die(6)
 die_3 = Die(6)
 die_list = [die_1, die_2, die_3]
-
-#sets rolls equal to an x amount of rolls (list)
 
 #change 10000 to desired amount of rolls
 rolls = die_1.record_rolls(die_list, 10000)
@@ -30,7 +27,6 @@ hist = pygal.Bar()
 hist.title = title
 hist.x_labels = x_list
 hist.y_title = "Frequency Results"
-#
+
 hist.add(die_1.dice_title, counted_rolls)
 hist.render_to_file('die_visual.svg')
-
